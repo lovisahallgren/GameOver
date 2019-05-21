d12912fe57e73cdf1d8dbd2f66a2a156f15193f0
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-05-21 12:28:49 +0200

    bubbles split in two when hit by bullet

--- src/index.js
+++ src/index.js
@@ -27,16 +27,19 @@ let score = 0;
 let scoreText;
 let player;
 let bubbles;
+let redbubbles;
 let bullets;
 let bullet;
 let cursors;
 let bubbleCount;
+let bubbleSplit = -1;
 
 function preload() {
   this.load.image('sky', '../src/assets/sky.png');
   this.load.image('ground', '../src/assets/ground.png');
   this.load.image('bullets', '../src/assets/bullet.png');
   this.load.image('bubble', '../src/assets/bubble.png');
+  this.load.image('redbubble', '../src/assets/redbubble.png');
   this.load.spritesheet('dude', '../src/assets/dude.png', {
     frameWidth: 32,
     frameHeight: 48
@@ -101,6 +104,16 @@ function create() {
     // }
   });
 
+  redbubbles = this.physics.add.group({
+    key: 'redbubble',
+    repeat: bubbleSplit
+    // setXY: {
+    //   x: bullet.x,
+    //   y: bullet.y,
+    //   stepX: 20
+    // }
+  });
+
   // bullet = this.physics.add.sprite({
   //     key: 'bullets',
   //     frames: [
@@ -112,6 +125,12 @@ function create() {
   //     frameRate: 10,
   //     repeat: 20
   //   });
+
+  // redbubbles.children.iterate(function(child) {
+  //   child.setBounce(1);
+  //   child.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  //   child.setCollideWorldBounds(true);
+  // });
 
   bubbles.children.iterate(function(child) {
     child.setBounce(1);
@@ -126,14 +145,18 @@ function create() {
 
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(bubbles, platforms);
+  this.physics.add.collider(redbubbles, platforms);
 
   this.physics.add.collider(player, bubbles, hitByBubble, null, this);
+  this.physics.add.collider(player, redbubbles, hitByBubble, null, this);
   this.physics.add.collider(bullet, platforms);
-  this.physics.add.overlap(bullet, bubbles, shootBubble, null, this);
+  this.physics.add.overlap(bullet, bubbles, shootBubble, splitBubble, this);
+  this.physics.add.overlap(bullet, redbubbles, shootBubble, null, this);
 }
 
 function update() {
   cursors = this.input.keyboard.createCursorKeys();
+  console.log(bullet.x, bullet.y);
 
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
@@ -164,13 +187,40 @@ function fireBullet() {
   bullet.body.setGravityY(-500);
 }
 
+function splitBubble(bullet, bubble) {
+  // if (this.physics.world.overlap(bullet, bubbles)) {
+  // let x =
+  //   player.x < 400
+  //     ? Phaser.Math.Between(400, 800)
+  //     : Phaser.Math.Between(0, 400);
+  // redbubbles.children.iterate(function(child) {
+  //   child.enableBody(true, Phaser.Math.Between(0, 800), 0, true, true);
+  //   child.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  bubbleSplit = 2;
+  // console.log(bubble);
+
+  let x = player.x;
+  let y = player.y - 200;
+  let redbubble = redbubbles.create(x, y, 'redbubble');
+  redbubble.setBounce(1);
+  redbubble.setCollideWorldBounds(true);
+  redbubble.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  // });
+  // }
+}
+
 function shootBubble(bullet, bubble) {
   bubble.disableBody(true, true);
 
   score += 10;
   scoreText.setText('Score: ' + score);
 
-  if (bubbles.countActive(true) === 0) {
+  if (this.physics.add.overlap(bullet, bubbles)) {
+    console.log(bubble.x, bubble.y);
+    splitBubble();
+  }
+
+  if (bubbles.countActive(true) === 0 && redbubbles.countActive(true) === 0) {
     bubbleCount += 1;
     setTimeout(() => {
       bubbles.children.iterate(function(child) {
